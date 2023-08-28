dd11fb3f4af05018473ab959e9c62aeec9d2993e
Leopoldo <poldosantiago@gmail.com>
2023-08-28 19:37:44 -0300

    teste 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const routes = require('./routes/routes');
 const app = express();
 routes(app);
 
-const porta = 3001;
+const porta = process.env.PORT || 3000;
 
 app.listen(porta, () => console.log(`- servidor ouvindo pela porta ${porta}\n- http://localhost:${porta}`));
 
