a60954588c4e19da9c8e96392c50e691671e66b2
Leopoldo <poldosantiago@gmail.com>
2023-08-28 21:26:35 -0300

    aaaaaa

--- config/multer.js
+++ config/multer.js
@@ -1,10 +1,12 @@
 const multer = require('multer');
 const path = require('path');
 
+console.log(path.join(__dirname, '/uploads/'))
+
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		cb(null, './uploads/')
-		// cb(null, path.join(__dirname, './uploads/'))
+		// cb(null, path.join(__dirname, '/uploads/'))
 	},
 	filename: (req, file, cb) => {
 		cb(null, Date.now() + path.extname(file.originalname))
